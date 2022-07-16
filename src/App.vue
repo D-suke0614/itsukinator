2d01a25d305b745dd491cfec17ed84e81f82aaf9
Daisuke <d.suke.kida@gmail.com>
2022-07-17 00:15:53 +0900

    [feat] nickname page, setting route

--- src/App.vue
+++ src/App.vue
@@ -1,58 +1,28 @@
 <template>
   <v-app>
-    <!-- <v-app-bar
-      app
-      color="primary"
-      dark
-    >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
-      >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
-    </v-app-bar> -->
-
+    <itsukiHeader/>
     <v-main>
-      <!-- <HelloWorld/> -->
-      <Home/>
+      <router-view></router-view>
+      <!-- <Home/> -->
+      <!-- <Nickname/> -->
     </v-main>
+    <itsukiFooter/>
   </v-app>
 </template>
 
 <script>
-// import HelloWorld from './components/HelloWorld';
-import Home from './components/pages/itsuki-home.vue'
+import itsukiHeader from './components/common/molecules/itsuki-header.vue'
+import itsukiFooter from './components/common/molecules/itsuki-footer.vue'
+// import Home from './components/pages/itsuki-home.vue'
+// import Nickname from './components/pages/itsuki-nickname.vue'
 export default {
   name: 'App',
 
   components: {
-    // HelloWorld,
-    Home,
+    // Home,
+    itsukiHeader,
+    itsukiFooter,
+    // Nickname,
   },
 
   data: () => ({
