fb703438b41a0d09d6b77b65b40ad98ad5e6d132
tai-namechan <s.namety.0106.hta1@gmail.com>
2022-07-17 23:34:16 +0900

    いつき

--- src/components/pages/itsuki-select-content.vue
+++ src/components/pages/itsuki-select-content.vue
@@ -2,11 +2,32 @@
   <div class="select-page">
     {{ $route.params.selected }}
     <h2>悩みはなんやねん？</h2>
-    <v-radio-group column class="radio-group" v-model="selected">
+    <v-radio-group v-if="$route.params.selected == 0" column class="radio-group" v-model="selected">
       <v-radio class="radio" label="90番目の女が見つからんねん" value="0"></v-radio>
       <v-radio class="radio" label="東京に女がおらんねん" value="1"></v-radio>
       <v-radio class="radio" label="Tinderやってると疑われてるねん" value="2"></v-radio>
       <v-radio class="radio" label="今日も朝帰りしてしもてん" value="3"></v-radio>
+    </v-radio-group>
+
+    <v-radio-group v-if="$route.params.selected == 1"  column class="radio-group" v-model="selected">
+      <v-radio class="radio" label="仕事辞めたいねん" value="0"></v-radio>
+      <v-radio class="radio" label="貢献できる自信ないねん" value="1"></v-radio>
+      <v-radio class="radio" label="社長が嫌いやねん" value="2"></v-radio>
+      <v-radio class="radio" label="通勤だるいねん" value="3"></v-radio>
+    </v-radio-group>
+
+    <v-radio-group v-if="$route.params.selected == 2"  column class="radio-group" v-model="selected">
+      <v-radio class="radio" label="友達が少ないねん" value="0"></v-radio>
+      <v-radio class="radio" label="あああ" value="1"></v-radio>
+      <v-radio class="radio" label="Tinderやってると疑われてるねん" value="2"></v-radio>
+      <v-radio class="radio" label="今日も朝帰りしてしもてん" value="3"></v-radio>
+    </v-radio-group>
+
+    <v-radio-group v-if="$route.params.selected == 3"  column class="radio-group" v-model="selected">
+      <v-radio class="radio" label="浪費癖がすごいねん" value="0"></v-radio>
+      <v-radio class="radio" label="s" value="1"></v-radio>
+      <v-radio class="radio" label="s" value="2"></v-radio>
+      <v-radio class="radio" label="d" value="3"></v-radio>
     </v-radio-group>
     <v-btn class="submit" @click="submit">次へ</v-btn>
   </div>
@@ -16,6 +37,7 @@
 export default {
   name: 'itsuki-select-content',
   data() {
+
     return {
 
     }
